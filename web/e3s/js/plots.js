76298b8809c807ccd230eb9a9ee8cfc1f80f1274
Louis Duchemin <ls.duchemin@gmail.com>
2018-07-14 12:51:49 +0200

    Ajout commentaires plots.js

--- web/e3s/js/plots.js
+++ web/e3s/js/plots.js
@@ -1,36 +1,50 @@
-function unpack(rows, key) {
-  return rows.map(function(row) { return row[key]; });
+/**
+ * Renvoie un tableau des valeurs d'une clé d'un objet JSON.
+ * @param {Object} json objet JSON
+ * @param {any} key clé à cibler
+ */
+function unpack(json, key) {
+  return json.map(function(row) { return row[key] })
 }
 
-
-function barPlot(target, rows) {
-  var methode = unpack(rows, 'methode'),
-    date_motu = unpack(rows, 'date_motu'),
-    labels = unpack(rows, 'label');
-  var counters = {
-    match: unpack(rows, 'match'),
-    split: unpack(rows, 'split'),
-    lump: unpack(rows, 'lump'),
-    reshuffling: unpack(rows, 'reshuffling')
+/**
+ * Affiche le diagramme en baton des rearrangements entre méthodes
+ * @param {string} target identifiant du contenant
+ * @param {Object} json données JSON
+ */
+function barPlot(target, json) {
+  // Extraction des données
+  const methode = unpack(json, 'methode'),
+    date_motu = unpack(json, 'date_motu'),
+    labels = unpack(json, 'label')
+  const counters = {
+    match: unpack(json, 'match'),
+    split: unpack(json, 'split'),
+    lump: unpack(json, 'lump'),
+    reshuffling: unpack(json, 'reshuffling'),
   }
 
-  var data = [];
-  for (var cnt in counters) {
+  // Constitution des données à afficher
+  var data = []
+  for (var cnt in counters) { // iteration
     if (counters.hasOwnProperty(cnt))
       data.push({
-        x: labels,
-        y: counters[cnt],
-        name: cnt,
-        type: 'bar'
+        x: labels, // label de méthode
+        y: counters[cnt], // comptages
+        name: cnt, // counters.key
+        type: 'bar',
       });
   }
 
+  // Construction objet plotly
   var d3 = Plotly.d3;
-  $(target).html('');
-  var gd3 = d3.select(target);
-  var gd = gd3.node();
+  $(target).html(''); // vidage du container
+  var gd3 = d3.select(target); // assignation du d3 au container
+  var gd = gd3.node(); // gd : objet graphique
 
-  var layout = {
+  // Paramètres d'affichage
+  const layout = {
+    // Axe des X
     xaxis: {
       title: "Méthode",
       titlefont: {
@@ -40,6 +54,7 @@ function barPlot(target, rows) {
       },
       fixedrange: true
     },
+    // Axe des Y
     yaxis: {
       title: "Réarrangements",
       titlefont: {
@@ -49,24 +64,30 @@ function barPlot(target, rows) {
       },
       fixedrange: true
     },
+    // Affichage du texte
     font: {
       family: 'sans serif',
       size: 14
     },
     showlegend: true,
-    barmode: 'group',
-  };
+    barmode: 'group', // affichage des barres côte à côte
+  }
 
+  // Déclaration du plot
+  Plotly.newPlot(
+    gd, // objet plotly
+    data, // données à construire
+    layout, // paramètres d'affichage
+    {
+      scrollZoom: false, // non zoomable
+      displaylogo: false, // pas de logo, merci
+      modeBarButtonsToRemove: [ // supprimer les boutons de contrôle superflus
+        'sendDataToCloud', 'box', 'lasso2d', 'select2d', 'pan2d',
+        'zoom2d', 'zoomIn2d', 'zoomOut2d', 'autoScale2d', 'resetScale2d'
+      ]
+    });
 
-  Plotly.newPlot(gd, data, layout, {
-    scrollZoom: false,
-    displaylogo: false,
-    modeBarButtonsToRemove: [
-      'sendDataToCloud', 'box', 'lasso2d', 'select2d', 'pan2d',
-      'zoom2d', 'zoomIn2d', 'zoomOut2d', 'autoScale2d', 'resetScale2d'
-    ]
-  });
-
+  // Responsive
   Plotly.Plots.resize(gd);
   window.onresize = function() {
     Plotly.Plots.resize(gd);
@@ -75,21 +96,35 @@ function barPlot(target, rows) {
 }
 
 
+/**
+ * Fonction d'affichage des graphiques d'échantillonnage COI
+ * 
+ * @param {Object} json_no_co1 
+ * @param {Object} json_co1 
+ * @param {number} lmp 
+ * @param {number} lmp_co1 
+ */
+function geoPlot(json_no_co1, json_co1, lmp = undefined, lmp_co1 = undefined) {
 
-function geoPlot(rows_no_co1, rows_co1, lmp = false, lmp_co1 = false) {
+  /**
+   * Fonction pour extraire les données JSON et construire un objet de données 
+   * pour plotly
+   * 
+   * @param {Object} json données json
+   * @param {Object} update données à ajouter
+   */
+  function build_station_data(json, update = {}) {
+    const taxon = unpack(json, 'taxon_id'),
+      taxname = unpack(json, 'taxname'),
+      code_station = unpack(json, 'code_station'),
+      latitude = unpack(json, 'latitude'),
+      longitude = unpack(json, 'longitude'),
+      altitude = unpack(json, 'altitude'),
+      commune = unpack(json, 'commune'),
+      pays = unpack(json, 'pays')
 
-  function build_station_data(rows, update = {}) {
-    var taxon = unpack(rows, 'taxon_id'),
-      taxname = unpack(rows, 'taxname'),
-      code_station = unpack(rows, 'code_station'),
-      latitude = unpack(rows, 'latitude'),
-      longitude = unpack(rows, 'longitude'),
-      altitude = unpack(rows, 'altitude'),
-      commune = unpack(rows, 'commune'),
-      pays = unpack(rows, 'pays');
-
-    var hoverText = [],
-      colors = [];
+    // Initialisation des hover text
+    var hoverText = []
     for (var i = 0; i < taxname.length; i++) {
       var stationText = [
         code_station[i],
@@ -97,12 +132,11 @@ function geoPlot(rows_no_co1, rows_co1, lmp = false, lmp_co1 = false) {
         "Alt:" + altitude[i] + "m",
         commune[i],
         pays[i]
-      ].join("<br>");
-      hoverText.push(stationText);
-      //colors.push(color);
+      ].join("<br>")
+      hoverText.push(stationText)
     }
 
-    var data = {
+    const data = {
       type: 'scattergeo',
       lat: latitude,
       lon: longitude,
@@ -118,26 +152,28 @@ function geoPlot(rows_no_co1, rows_co1, lmp = false, lmp_co1 = false) {
       name: "Stations",
     }
 
-    $.extend(true, data, update);
-    return data;
+    // Ajout données supplémentaires à l'objet data
+    $.extend(true, data, update)
+    return data
   }
-  var d3 = Plotly.d3;
 
-  var WIDTH_IN_PERCENT_OF_PARENT = "100",
-    HEIGHT_IN_PERCENT_OF_PARENT = "100";
-  $("#station-geo-map").html('');
-  var gd3 = d3.select('#station-geo-map');
-  var gd = gd3.node();
+  // Init plotly
+  var d3 = Plotly.d3
+  $("#station-geo-map").html('')
+  var gd3 = d3.select('#station-geo-map')
+  var gd = gd3.node()
 
-  var data_co1 = build_station_data(rows_co1, {
+  // Données de COI
+  const data_co1 = build_station_data(json_co1, {
     name: "Stations COI",
     marker: {
       symbol: "triangle-up",
       color: "red"
     }
-  });
+  })
 
-  var data_no_co1 = build_station_data(rows_no_co1, {
+  // Données non COI
+  const data_no_co1 = build_station_data(json_no_co1, {
     name: "Stations Lot Mat.",
     marker: {
       symbol: "circle-open",
@@ -149,12 +185,15 @@ function geoPlot(rows_no_co1, rows_co1, lmp = false, lmp_co1 = false) {
         color: "green",
       }
     }
-  });
+  })
 
+  // Objet data : contient les scatterplots
   var data = [
     data_co1,
-    data_no_co1
+    data_no_co1,
   ]
+
+  // Coordonnées de la ligne LMP
   if (lmp) {
     data.push({
       type: 'scattergeo',
@@ -168,9 +207,10 @@ function geoPlot(rows_no_co1, rows_co1, lmp = false, lmp_co1 = false) {
         dash: 'dash'
       },
       name: "Lat. Mid-Point (LM)"
-    });
+    })
   }
 
+  // Coordonnées de la ligne LMP COI
   if (lmp_co1) {
     data.push({
       type: 'scattergeo',
@@ -184,10 +224,13 @@ function geoPlot(rows_no_co1, rows_co1, lmp = false, lmp_co1 = false) {
         dash: 'dash'
       },
       name: "Lat. Mid-Point (COI)"
-    });
+    })
   }
 
-  var layout = {
+  // Objet data complet : scatterplots + LMP + LMP COI
+
+  // Paramètres d'affichage du graphique
+  const layout = {
     font: {
       family: 'Droid Serif, serif',
       size: 14
@@ -203,7 +246,7 @@ function geoPlot(rows_no_co1, rows_co1, lmp = false, lmp_co1 = false) {
       b: 0
     },
     showlegend: true,
-    geo: {
+    geo: { // carte geographique
       scope: 'world',
       resolution: 50,
       projection: {
@@ -225,37 +268,43 @@ function geoPlot(rows_no_co1, rows_co1, lmp = false, lmp_co1 = false) {
       framewidth: 2,
       bgcolor: 'lightgrey'
     }
-  };
-
+  }
 
   Plotly.newPlot(gd, data, layout, {
-    displaylogo: false,
+    displaylogo: false, // pas de logo, enlever boutons de controle inutiles
     modeBarButtonsToRemove: ['sendDataToCloud', 'box', 'lasso2d', 'select2d', 'pan2d']
-  });
+  })
 
-  Plotly.Plots.resize(gd);
-  return gd;
+  Plotly.Plots.resize(gd) // Remplir l'espace dans le DOM
+
+  return gd // Renvoi objet plotly
 }
 
 
-function motuGeoPlot(rows) {
+/**
+ * Affichage des MOTUs sur la carte geographique
+ * 
+ * @param {Object} json 
+ */
+function motuGeoPlot(json) {
+  /**
+   * Fonction pour extraire les données JSON et construire un objet de données 
+   * pour plotly
+   * 
+   * @param {Object} json données json
+   * @param {Object} update données à ajouter
+   */
+  function build_station_data(json, update = {}) {
+    const taxname = unpack(json, 'taxname'),
+      code_station = unpack(json, 'code_station'),
+      latitude = unpack(json, 'latitude'),
+      longitude = unpack(json, 'longitude'),
+      altitude = unpack(json, 'altitude'),
+      commune = unpack(json, 'commune'),
+      pays = unpack(json, 'pays'),
+      motus = unpack(json, 'motu')
 
-  function unpack(rows, key) {
-    return rows.map(function(row) { return row[key]; });
-  }
-
-  function build_station_data(rows, update = {}) {
-    var taxname = unpack(rows, 'taxname'),
-      code_station = unpack(rows, 'code_station'),
-      latitude = unpack(rows, 'latitude'),
-      longitude = unpack(rows, 'longitude'),
-      altitude = unpack(rows, 'altitude'),
-      commune = unpack(rows, 'commune'),
-      pays = unpack(rows, 'pays'),
-      motus = unpack(rows, 'motu');
-
-    var hoverText = [],
-      colors = [];
+    var hoverText = []
     for (var i = 0; i < taxname.length; i++) {
       var stationText = [
         "MOTU " + motus[i],
@@ -264,9 +313,8 @@ function motuGeoPlot(rows) {
         "Alt:" + altitude[i] + "m",
         commune[i],
         pays[i]
-      ].join("<br>");
-      hoverText.push(stationText);
-      //colors.push(color);
+      ].join("<br>")
+      hoverText.push(stationText)
     }
 
     var data = {
@@ -281,28 +329,28 @@ function motuGeoPlot(rows) {
           width: 1,
         }
       },
-      name: "MOTU " + rows[0].motu,
+      name: "MOTU " + json[0].motu,
     }
 
-    $.extend(true, data, update);
-    return data;
+    $.extend(true, data, update)
+    return data
   }
 
-
-  var motu_map = {};
-
-  for (var i = 0; i < rows.length; i++) {
+  // Trie le json par motu : idstation : jsondata
+  var motu_map = {}
+  for (var i = 0; i < json.length; i++) {
     $.extend(true, motu_map, {
-      [rows[i].motu]: {
-        [rows[i].id_sta]: rows[i]
+      [json[i].motu]: {
+        [json[i].id_sta]: json[i]
       }
     })
   }
 
-  var traces = [];
-  var i = 0;
+  // Création d'une trace par motu : différentes couleurs
+  var traces = []
+  var i = 0
   for (var motu in motu_map) {
-    i += 1;
+    i += 1
     if (motu_map.hasOwnProperty(motu)) {
       traces.push(build_station_data(
         Object.values(motu_map[motu]), {
@@ -311,21 +359,17 @@ function motuGeoPlot(rows) {
             size: 8
           },
           opacity: 0.7
-        }));
+        }))
     }
   }
 
-  var d3 = Plotly.d3;
 
-  var WIDTH_IN_PERCENT_OF_PARENT = "100",
-    HEIGHT_IN_PERCENT_OF_PARENT = "100";
+  var d3 = Plotly.d3
+  $("#motu-geo-map").html('')
+  var gd3 = d3.select('#motu-geo-map')
+  var gd = gd3.node()
 
-
-  $("#motu-geo-map").html('');
-  var gd3 = d3.select('#motu-geo-map');
-  var gd = gd3.node();
-
-  var layout = {
+  const layout = {
     font: {
       family: 'Droid Serif, serif',
       size: 14
@@ -363,20 +407,22 @@ function motuGeoPlot(rows) {
       framewidth: 2,
       bgcolor: 'lightgrey'
     }
-  };
+  }
 
 
   Plotly.newPlot(gd, traces, layout, {
     displaylogo: false,
     modeBarButtonsToRemove: ['sendDataToCloud', 'box', 'lasso2d', 'select2d', 'pan2d']
-  });
+  })
 
-  Plotly.Plots.resize(gd);
+  // Responsive mode
+  Plotly.Plots.resize(gd)
   window.onresize = function() {
-    $(".geo-overlay").show();
+    $(".geo-overlay").show()
     Plotly.Plots.resize(gd).then(function() {
-      $(".geo-overlay").hide();
-    });
-  };
-  return gd;
+      $(".geo-overlay").hide()
+    })
+  }
+
+  return gd // renvoi objet plotly
 }
