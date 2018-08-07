c0f41b3879d0877df5cbc0892233d448a881ce37
Louis <louis.duchemin@etu.univ-lyon1.fr>
2018-08-07 12:51:52 +0200

    Tweak carte stations dashboard pour accélérer rendu

--- web/e3s/js/requetes/plots.js
+++ web/e3s/js/requetes/plots.js
@@ -144,45 +144,7 @@ class BarPlot extends BasePlot {
 class BaseGeoPlot extends BasePlot {
   constructor(containerId) {
     super(containerId)
-    this.layout = {
-      font: {
-        family: 'Droid Serif, serif',
-        size: 14
-      },
-      titlefont: {
-        size: 16
-      },
-      height: 600,
-      margin: {
-        l: 0,
-        r: 0,
-        t: 15,
-        b: 0
-      },
-      showlegend: true,
-      geo: { // carte geographique
-        scope: 'world',
-        resolution: 30,
-        projection: {
-          type: 'miller'
-        },
-        showrivers: true,
-        rivercolor: 'lightblue',
-        showlakes: true,
-        lakecolor: 'lightblue',
-        showland: true,
-        landcolor: '#E0F8F7',
-        countrycolor: 'grey',
-        countrywidth: 1,
-        subunitcolor: '#d3d3d3',
-        showocean: true,
-        oceancolor: 'lightblue',
-        showframe: true,
-        framecolor: '#000',
-        framewidth: 2,
-        bgcolor: 'lightgrey'
-      }
-    }
+    this.layout = plotlyconfig.geo.layout // from options.js
   }
 
   resize() {
