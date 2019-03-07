1906583bd8e4b2b6e70846eb6f8003495aff33a3
Louis Duchemin <louis.duchemin@etu.univ-lyon1.fr>
2019-03-07 10:48:43 +0100

    Update licence headers of SpeciesSearchBundle

--- src/Lehna/SpeciesSearchBundle/Controller/TaxonConsistencyController.php
+++ src/Lehna/SpeciesSearchBundle/Controller/TaxonConsistencyController.php
@@ -5,13 +5,13 @@
  *
  * Authors : see information concerning authors of GOTIT project in file AUTHORS.md
  *
- * E3sBundle is free software : you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
+ * SpeciesSearchBundle is free software : you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * 
  * E3sBundle is distributed in the hope that it will be useful,but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License along with E3sBundle.  If not, see <https://www.gnu.org/licenses/>
+ * You should have received a copy of the GNU General Public License along with SpeciesSearchBundle.  If not, see <https://www.gnu.org/licenses/>
  *
  */
 
