51b5c1c6a951d892c8be7808c00e5dd9466d5f27
Louis Duchemin <ls.duchemin@gmail.com>
2019-09-25 15:03:53 +0200

    Local JS modules instead of using CDN * ESRI Leaflet * Leaflet fullscreen * Leaflet easybutton

--- src/Lehna/SpeciesSearchBundle/Resources/public/js/co1_sampling/results.js
+++ src/Lehna/SpeciesSearchBundle/Resources/public/js/co1_sampling/results.js
@@ -6,88 +6,88 @@ import { dtconfig, linkify } from '../datatables_utils.js'
  * @param {String} tableId result table DOM id
  */
 function initDataTable(tableId, drawCallback) {
-    if (!$.fn.DataTable.isDataTable(tableId)) {
-        fetchCurrentUser()
-            .then(response => response.json())
-            .then(user => {
-                const dtbuttons = user.role === 'ROLE_INVITED' ? [] : dtconfig.buttons
-                const table = $(tableId)
-                // Render floats with precision 3
-                const renderNumber = $.fn.dataTable.render.number('', '.', 3)
+  if (!$.fn.DataTable.isDataTable(tableId)) {
+    fetchCurrentUser()
+      .then(response => response.json())
+      .then(user => {
+        const dtbuttons = user.role === 'ROLE_INVITED' ? [] : dtconfig.buttons
+        const table = $(tableId)
+        // Render floats with precision 3
+        const renderNumber = $.fn.dataTable.render.number('', '.', 3)
 
-                table.DataTable({
-                    autoWidth: false,
-                    responsive: {
-                        orthogonal: "responsive",
-                        details: {
-                            type: 'column'
-                        }
-                    },
-                    ajax: {
-                        "url": Routing.generate('co1-sampling-query'),
-                        "type": "POST",
-                        "dataSrc": "",
-                        "data": _ => {
-                            return $("#main-form").serialize()
-                        }
-                    },
-                    language: dtconfig.language[$("html").attr("lang")],
-                    dom: "lfrtipB",
-                    buttons: dtbuttons,
-                    order: [1, 'asc'],
-                    columns: [
-                        dtconfig.expandColumn,
-                        {
-                            data: "taxname",
-                            render: linkify("referentieltaxon_show", { col: 'id' })
-                        }, {
-                            data: "nb_sta"
-                        }, {
-                            data: "lmp",
-                            render: renderNumber,
-                            defaultContent: ""
-                        }, {
-                            data: "mle",
-                            render: renderNumber,
-                            defaultContent: ""
-                        }, {
-                            data: "nb_sta_co1"
-                        }, {
-                            data: "lmp_co1",
-                            render: renderNumber,
-                            defaultContent: ""
-                        }, {
-                            data: "mle_co1",
-                            render: renderNumber,
-                            defaultContent: ""
-                        }, {
-                            title: "<i class='fa fa-map-marker' style='margin-left:7px'></i>",
-                            data: "id",
-                            orderable: false,
-                            render: (data, type, row) =>
-                                Mustache.render($("#details-form-template").html(), row)
-                        }, {
-                            title: "<i class='fa fa-download' style='margin-left:7px'></i>",
-                            orderable: false,
-                            data: "id",
-                            render: (data, type, row) =>
-                                Mustache.render($("#download-form-template").html(), row)
-                        }
-                    ],
+        table.DataTable({
+          autoWidth: false,
+          responsive: {
+            orthogonal: "responsive",
+            details: {
+              type: 'column'
+            }
+          },
+          ajax: {
+            "url": Routing.generate('co1-sampling-query'),
+            "type": "POST",
+            "dataSrc": "",
+            "data": _ => {
+              return $("#main-form").serialize()
+            }
+          },
+          language: dtconfig.language[$("html").attr("lang")],
+          dom: "lfrtipB",
+          buttons: dtbuttons,
+          order: [1, 'asc'],
+          columns: [
+            dtconfig.expandColumn,
+            {
+              data: "taxname",
+              render: linkify("referentieltaxon_show", { col: 'id' })
+            }, {
+              data: "nb_sta"
+            }, {
+              data: "lmp",
+              render: renderNumber,
+              defaultContent: ""
+            }, {
+              data: "mle",
+              render: renderNumber,
+              defaultContent: ""
+            }, {
+              data: "nb_sta_co1"
+            }, {
+              data: "lmp_co1",
+              render: renderNumber,
+              defaultContent: ""
+            }, {
+              data: "mle_co1",
+              render: renderNumber,
+              defaultContent: ""
+            }, {
+              title: "<i class='fa fa-map-marker' style='margin-left:7px'></i>",
+              data: "id",
+              orderable: false,
+              render: (data, type, row) =>
+                Mustache.render($("#details-form-template").html(), row)
+            }, {
+              title: "<i class='fa fa-download' style='margin-left:7px'></i>",
+              orderable: false,
+              data: "id",
+              render: (data, type, row) =>
+                Mustache.render($("#download-form-template").html(), row)
+            }
+          ],
 
-                    drawCallback: drawCallback
-                }) // datatables
+          drawCallback: drawCallback
+        }) // datatables
 
-                /****************************
-                 * Submit form handler
-                 ************************** */
-                $("#main-form").submit(event => {
-                    event.preventDefault()
-                    $(this).find("button[type='submit']").button('loading')
-                    table.DataTable().ajax.reload()
-                })
-            })
-    }
+        /****************************
+         * Submit form handler
+         ************************** */
+        $("#main-form").submit(event => {
+          event.preventDefault()
+          $(this).find("button[type='submit']").button('loading')
+          table.DataTable().ajax.reload()
+        })
+      })
+  }
 }
 
 export { initDataTable }
--- src/Lehna/SpeciesSearchBundle/Resources/public/js/leaflet-style-controls.js
+++ src/Lehna/SpeciesSearchBundle/Resources/public/js/leaflet-style-controls.js
@@ -20,56 +20,56 @@
  */
 
 L.Control.StyleControl = L.Control.extend({
-    onAdd: function (map) {
-        let control = L.DomUtil.create("div", "leaflet-control-collapse leaflet-control-layers leaflet-control")
-        let icon = L.DomUtil.create("i", "fa fa-cog fa-2x", control)
-        let panel = L.DomUtil.create("div", "map-control-bar leaflet-control-layers-list", control)
-        L.DomEvent.disableClickPropagation(panel);
-        // Radius slider
-        let radiusLabel = L.DomUtil.create("label", "leaflet-ctrl-label", panel)
-        radiusLabel.innerHTML = Translator.trans("maps.controls.radius")
-        let radiusCtnr = L.DomUtil.create("div", "leaflet-slider-container", panel)
-        this.radiusSlider = L.DomUtil.create("input", "leaflet-slider", radiusCtnr)
-        Object.assign(this.radiusSlider, {
-            type: "range",
-            min: 3,
-            max: 12,
-            value: 5
-        })
-        // Opacity slider
-        let opacityLabel = L.DomUtil.create("label", "leaflet-ctrl-label", panel)
-        opacityLabel.innerHTML = Translator.trans("maps.controls.opacity")
-        let opacityCtnr = L.DomUtil.create("div", "leaflet-slider-container", panel)
-        this.opacitySlider = L.DomUtil.create("input", "leaflet-slider", opacityCtnr)
-        Object.assign(this.opacitySlider, {
-            type: "range",
-            min: 0.1,
-            max: 1,
-            step: 0.05,
-            value: 0.75
-        })
-        $(control).mouseover(_ => {
-            $(panel).css("display", "grid")
-            $(icon).hide()
-        })
-        $(control).mouseout(event => {
-            let e = event.toElement || event.relatedTarget;
-            if (e == this.radiusSlider || e == this.opacitySlider) {
-                return;
-            }
-            $(panel).css("display", "none")
-            $(icon).show()
-        })
-        return control
-    },
+  onAdd: function (map) {
+    let control = L.DomUtil.create("div", "leaflet-control-collapse leaflet-control-layers leaflet-control")
+    let icon = L.DomUtil.create("i", "fa fa-cog fa-2x", control)
+    let panel = L.DomUtil.create("div", "map-control-bar leaflet-control-layers-list", control)
+    L.DomEvent.disableClickPropagation(panel);
+    // Radius slider
+    let radiusLabel = L.DomUtil.create("label", "leaflet-ctrl-label", panel)
+    radiusLabel.innerHTML = Translator.trans("maps.controls.radius")
+    let radiusCtnr = L.DomUtil.create("div", "leaflet-slider-container", panel)
+    this.radiusSlider = L.DomUtil.create("input", "leaflet-slider", radiusCtnr)
+    Object.assign(this.radiusSlider, {
+      type: "range",
+      min: 3,
+      max: 12,
+      value: 5
+    })
+    // Opacity slider
+    let opacityLabel = L.DomUtil.create("label", "leaflet-ctrl-label", panel)
+    opacityLabel.innerHTML = Translator.trans("maps.controls.opacity")
+    let opacityCtnr = L.DomUtil.create("div", "leaflet-slider-container", panel)
+    this.opacitySlider = L.DomUtil.create("input", "leaflet-slider", opacityCtnr)
+    Object.assign(this.opacitySlider, {
+      type: "range",
+      min: 0.1,
+      max: 1,
+      step: 0.05,
+      value: 0.75
+    })
+    $(control).mouseover(_ => {
+      $(panel).css("display", "grid")
+      $(icon).hide()
+    })
+    $(control).mouseout(event => {
+      let e = event.toElement || event.relatedTarget;
+      if (e == this.radiusSlider || e == this.opacitySlider) {
+        return;
+      }
+      $(panel).css("display", "none")
+      $(icon).show()
+    })
+    return control
+  },
 
-    onRemove: function (map) {
-        // Nothing to do here
-    }
+  onRemove: function (map) {
+    // Nothing to do here
+  }
 });
 
 
 export function styleControl(opts) {
-    return new L.Control.StyleControl(opts);
+  return new L.Control.StyleControl(opts);
 }
 
