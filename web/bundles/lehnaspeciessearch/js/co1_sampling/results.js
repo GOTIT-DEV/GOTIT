5c0b4791cce6fb812066421786ad9f8f60d27efb
Louis Duchemin <ls.duchemin@gmail.com>
2019-10-02 20:20:25 +0200

    [SpeciesSearch] Fix CO1 results CSV export Disable for invited role

--- web/bundles/lehnaspeciessearch/js/co1_sampling/results.js
+++ web/bundles/lehnaspeciessearch/js/co1_sampling/results.js
@@ -1,19 +1,71 @@
 
 import { fetchCurrentUser } from '../utils.js'
 import { dtconfig, linkify } from '../datatables_utils.js'
+
+
+// Render floats with precision 3
+const renderNumber = $.fn.dataTable.render.number('', '.', 3)
+
+// Result table columns definition
+let columns = [
+  dtconfig.expandColumn,
+  {
+    data: "taxname",
+    render: linkify("referentieltaxon_show", { col: 'id' })
+  }, {
+    data: "nb_sta"
+  }, {
+    data: "lmp",
+    render: renderNumber,
+    defaultContent: ""
+  }, {
+    data: "mle",
+    render: renderNumber,
+    defaultContent: ""
+  }, {
+    data: "nb_sta_co1"
+  }, {
+    data: "lmp_co1",
+    render: renderNumber,
+    defaultContent: ""
+  }, {
+    data: "mle_co1",
+    render: renderNumber,
+    defaultContent: ""
+  }, {
+    title: "<i class='fas fa-map-marker' style='margin-left:7px'></i>",
+    data: "id",
+    orderable: false,
+    render: (data, type, row) =>
+      Mustache.render($("#details-form-template").html(), row)
+  }
+]
+
 /**
  * Initializes Datatable on result table DOM element
  * @param {String} tableId result table DOM id
  */
 function initDataTable(tableId, drawCallback) {
+  
+  const table = $(tableId)
+
   if (!$.fn.DataTable.isDataTable(tableId)) {
     fetchCurrentUser()
       .then(response => response.json())
       .then(user => {
+
+        // Allow result table download to authorized users
         const dtbuttons = user.role === 'ROLE_INVITED' ? [] : dtconfig.buttons
-        const table = $(tableId)
-        // Render floats with precision 3
-        const renderNumber = $.fn.dataTable.render.number('', '.', 3)
+
+        // Show download column to authorized users 
+        if (user.role !== 'ROLE_INVITED')
+          columns.push({
+            title: "<i class='fas fa-download' style='margin-left:7px'></i>",
+            orderable: false,
+            data: "id",
+            render: (data, type, row) =>
+              Mustache.render($("#download-form-template").html(), row)
+          })
 
         table.DataTable({
           autoWidth: false,
@@ -35,45 +87,7 @@ function initDataTable(tableId, drawCallback) {
           dom: "lfrtipB",
           buttons: dtbuttons,
           order: [1, 'asc'],
-          columns: [
-            dtconfig.expandColumn,
-            {
-              data: "taxname",
-              render: linkify("referentieltaxon_show", { col: 'id' })
-            }, {
-              data: "nb_sta"
-            }, {
-              data: "lmp",
-              render: renderNumber,
-              defaultContent: ""
-            }, {
-              data: "mle",
-              render: renderNumber,
-              defaultContent: ""
-            }, {
-              data: "nb_sta_co1"
-            }, {
-              data: "lmp_co1",
-              render: renderNumber,
-              defaultContent: ""
-            }, {
-              data: "mle_co1",
-              render: renderNumber,
-              defaultContent: ""
-            }, {
-              title: "<i class='fas fa-map-marker' style='margin-left:7px'></i>",
-              data: "id",
-              orderable: false,
-              render: (data, type, row) =>
-                Mustache.render($("#details-form-template").html(), row)
-            }, {
-              title: "<i class='fas fa-download' style='margin-left:7px'></i>",
-              orderable: false,
-              data: "id",
-              render: (data, type, row) =>
-                Mustache.render($("#download-form-template").html(), row)
-            }
-          ],
+          columns: columns,
 
           drawCallback: drawCallback
         }) // datatables
