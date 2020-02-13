ffd4928ae2f16d2f0a58e3846db707722e04ab98
Elsa <elsa-marlene.mendes-dos-ramos-varanda@etu.univ-lyon1.fr>
2020-02-13 12:11:19 +0100

    base_controller changes

--- src/Lehna/QueryBuilderBundle/Controller/DefaultController.php
+++ src/Lehna/QueryBuilderBundle/Controller/DefaultController.php
@@ -3,9 +3,22 @@
 namespace Lehna\QueryBuilderBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
+/**
+ * Controller for querying COI sampling coverage
+ *
+ * @Route("/")
+ * @Security("has_role('ROLE_INVITED')")
+ * @author Louis Duchemin <ls.duchemin@gmail.com>
+ */
 class DefaultController extends Controller
 {
+    /**
+   * @Route("/", name="query_index")
+   * Index : render query form interface
+   */
     public function indexAction()
     {
         return $this->render('@LehnaQueryBuilder/Default/index.html.twig');
