4c69828e2820f7f2c03a6d5e43697eff71ebcccd
Louis D <louis.duchemin@univ-lyon1.fr>
2020-11-02 12:23:47 +0100

    Dedicated autocompletion type + choice loader

---
+++ src/Form/ChoiceLoader/AutocompleteChoiceLoader.php
@@ -0,0 +1,58 @@
+<?php
+
+// https://stackoverflow.com/questions/35456199/symfony-2-8-dynamic-choicetype-options
+
+namespace App\Form\ChoiceLoader;
+
+use Symfony\Component\Form\ChoiceList\Loader\ChoiceLoaderInterface;
+use Symfony\Component\Form\ChoiceList\ChoiceListInterface;
+use Symfony\Component\Form\ChoiceList\ArrayChoiceList;
+use Doctrine\ORM\EntityRepository;
+
+class AutocompleteChoiceLoader implements ChoiceLoaderInterface {
+  /** @var ChoiceListInterface */
+  private $choiceList;
+  protected $repository;
+  protected $choice_value;
+
+  public function __construct(EntityRepository $er, String $choice_value) {
+    $this->repository   = $er;
+    $this->choice_value = $choice_value;
+  }
+
+  public function loadValuesForChoices(array $choices, $value = null) {
+    $choices = array_filter($choices, function ($c) {return $c != null;});
+
+    $values = array();
+    foreach ($choices as $key => $choice) {
+      if (is_callable($value)) {
+        $values[$key] = (string) call_user_func($value, $choice, $key);
+      } else {
+        $values[$key] = $choice;
+      }
+    }
+
+    $this->choiceList = new ArrayChoiceList($choices, $value);
+    return $values;
+  }
+
+  public function loadChoiceList($value = null) {
+    // is called on form view create after loadValuesForChoices of form create
+    if ($this->choiceList instanceof ChoiceListInterface) {
+      return $this->choiceList;
+    }
+
+    // if no values preset yet return empty list
+    $this->choiceList = new ArrayChoiceList([], $value);
+
+    return $this->choiceList;
+  }
+
+  public function loadChoicesForValues(array $values, $value = null) {
+
+    $choices = $this->repository->findBy([$this->choice_value => $values]);
+    dump('CHOICES', $choices);
+    $this->choiceList = new ArrayChoiceList($choices, $value);
+    return $choices;
+  }
+}
---
+++ src/Form/Type/SearchableSelectType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form\Type;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\OptionsResolver\Options;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Form\ChoiceLoader\AutocompleteChoiceLoader;
+
+class SearchableSelectType extends AbstractType {
+
+  public function __construct(EntityManagerInterface $em) {
+    $this->entityManager = $em;
+  }
+
+  public function configureOptions(OptionsResolver $resolver) {
+
+    $resolver->setRequired(["choice_label", "class"]);
+
+    $resolver->setDefaults([
+      'multiple'      => false,
+      'expanded'      => false,
+      'choice_value'  => 'id',
+      /* Load choices using preset data
+       * Also add choices generated from autocompletion on client side
+       */
+      'choice_loader' => function (Options $options) {
+        $entityRepository = $this->entityManager->getRepository($options['class']);
+        return new AutocompleteChoiceLoader($entityRepository, $options['choice_value']);
+      },
+      // Disable fetching entities in querybuilder
+      'query_builder' => function (EntityRepository $er) {
+        return $er->createQueryBuilder('x')->where('x.id is NULL');
+      },
+    ]);
+
+    $resolver->setNormalizer('attr', function (Options $options, $value) {
+      return array_merge($value, [
+        'class'                     => ($attrs['class'] ?? "") . " remote-source",
+        'data-minimum-input-length' => 1,
+      ]);
+    });
+  }
+
+  public function getParent() {
+    return EntityType::class;
+  }
+}
