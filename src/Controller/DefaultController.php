884e51c91bdd01fa23bb2a97834b2d8ecaf48f14
Louis D <louis.duchemin@univ-lyon1.fr>
2021-01-04 16:32:21 +0100

    Legal notice pages

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DefaultController extends AbstractController {
+  /**
+   * @Route("/legals/", name="legals")
+   */
+  public function legals(Request $request) {
+    return $this->render("misc/legal-notices." . $request->getLocale() . ".html.twig");
+  }
+}
