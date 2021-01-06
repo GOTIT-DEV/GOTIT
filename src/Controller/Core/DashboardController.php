1da05da4c55400b84bf1ba505b145310ba1c9c1b
Louis D <louis.duchemin@univ-lyon1.fr>
2021-01-06 15:25:33 +0100

    Reworked login page
    Changes main title to World Asellidae Database

--- src/Controller/Core/DashboardController.php
+++ src/Controller/Core/DashboardController.php
@@ -17,17 +17,15 @@
 
 namespace App\Controller\Core;
 
-use App\Entity\Station;
 use App\Services\Core\GenericFunctionE3s;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends AbstractController {
   /**
-   * @Route("/", name="default_index")
+   * @Route("/", name="dashboard")
    * @Security("has_role('ROLE_INVITED')")
    * @author Philippe Grison  <philippe.grison@mnhn.fr>
    */
@@ -35,23 +33,23 @@ class DashboardController extends AbstractController {
     // load Doctrine Manager
     $em = $this->getDoctrine()->getManager();
     //
-    $nbcollectes            = $em->createQuery('SELECT COUNT(u.id) FROM App:Collecte u')->getSingleScalarResult();
-    $nbstations             = $em->createQuery('SELECT COUNT(u.id) FROM App:Station u')->getSingleScalarResult();
-    $nbLotMateriel          = $em->createQuery('SELECT COUNT(u.id) FROM App:LotMateriel u')->getSingleScalarResult();
-    $nbLotMaterielExt       = $em->createQuery('SELECT COUNT(u.id) FROM App:LotMaterielExt u')->getSingleScalarResult();
-    $nbIndividu             = $em->createQuery('SELECT COUNT(u.id) FROM App:Individu u')->getSingleScalarResult();
-    $nbIndividuLame         = $em->createQuery('SELECT COUNT(u.id) FROM App:IndividuLame u')->getSingleScalarResult();
-    $nbAdn                  = $em->createQuery('SELECT COUNT(u.id) FROM App:Adn u')->getSingleScalarResult();
-    $nbPcr                  = $em->createQuery('SELECT COUNT(u.id) FROM App:Pcr u')->getSingleScalarResult();
-    $nbChromatogramme       = $em->createQuery('SELECT COUNT(u.id) FROM App:Chromatogramme u')->getSingleScalarResult();
-    $nbSequenceAssemblee    = $em->createQuery('SELECT COUNT(u.id) FROM App:SequenceAssemblee u')->getSingleScalarResult();
+    $nbcollectes = $em->createQuery('SELECT COUNT(u.id) FROM App:Collecte u')->getSingleScalarResult();
+    $nbstations = $em->createQuery('SELECT COUNT(u.id) FROM App:Station u')->getSingleScalarResult();
+    $nbLotMateriel = $em->createQuery('SELECT COUNT(u.id) FROM App:LotMateriel u')->getSingleScalarResult();
+    $nbLotMaterielExt = $em->createQuery('SELECT COUNT(u.id) FROM App:LotMaterielExt u')->getSingleScalarResult();
+    $nbIndividu = $em->createQuery('SELECT COUNT(u.id) FROM App:Individu u')->getSingleScalarResult();
+    $nbIndividuLame = $em->createQuery('SELECT COUNT(u.id) FROM App:IndividuLame u')->getSingleScalarResult();
+    $nbAdn = $em->createQuery('SELECT COUNT(u.id) FROM App:Adn u')->getSingleScalarResult();
+    $nbPcr = $em->createQuery('SELECT COUNT(u.id) FROM App:Pcr u')->getSingleScalarResult();
+    $nbChromatogramme = $em->createQuery('SELECT COUNT(u.id) FROM App:Chromatogramme u')->getSingleScalarResult();
+    $nbSequenceAssemblee = $em->createQuery('SELECT COUNT(u.id) FROM App:SequenceAssemblee u')->getSingleScalarResult();
     $nbSequenceAssembleeExt = $em->createQuery('SELECT COUNT(u.id) FROM App:SequenceAssembleeExt u')->getSingleScalarResult();
-    $nbMotu                 = $em->createQuery('SELECT COUNT(u.id) FROM App:Assigne u')->getSingleScalarResult();
-    $nbMotuSqcAss           = count($em->createQuery('SELECT COUNT(sa.id) FROM App:Assigne u JOIN u.sequenceAssembleeFk sa GROUP BY sa.id')->getResult());
-    $nbMotuSqcAssExt        = count($em->createQuery('SELECT COUNT(sae.id) FROM App:Assigne u JOIN u.sequenceAssembleeExtFk sae GROUP BY sae.id')->getResult());
-    $nbBoite                = $em->createQuery('SELECT COUNT(u.id) FROM App:Boite u')->getSingleScalarResult();
-    $nbSource               = $em->createQuery('SELECT COUNT(u.id) FROM App:Source u')->getSingleScalarResult();
-    $nbTaxon                = count($em->createQuery('SELECT COUNT(rt.id) FROM App:EspeceIdentifiee u JOIN u.referentielTaxonFk rt GROUP BY rt.id')->getResult());
+    $nbMotu = $em->createQuery('SELECT COUNT(u.id) FROM App:Assigne u')->getSingleScalarResult();
+    $nbMotuSqcAss = count($em->createQuery('SELECT COUNT(sa.id) FROM App:Assigne u JOIN u.sequenceAssembleeFk sa GROUP BY sa.id')->getResult());
+    $nbMotuSqcAssExt = count($em->createQuery('SELECT COUNT(sae.id) FROM App:Assigne u JOIN u.sequenceAssembleeExtFk sae GROUP BY sae.id')->getResult());
+    $nbBoite = $em->createQuery('SELECT COUNT(u.id) FROM App:Boite u')->getSingleScalarResult();
+    $nbSource = $em->createQuery('SELECT COUNT(u.id) FROM App:Source u')->getSingleScalarResult();
+    $nbTaxon = count($em->createQuery('SELECT COUNT(rt.id) FROM App:EspeceIdentifiee u JOIN u.referentielTaxonFk rt GROUP BY rt.id')->getResult());
     //
     $tab_toshow = [];
     // returns the last records of the dna
@@ -63,11 +61,11 @@ class DashboardController extends AbstractController {
     foreach ($entities_toshow as $entity) {
       $id = $entity->getId();
       //$DateCre = ($entity->getDateCre() !== null) ?  $entity->getDateCre()->format('Y-m-d H:i:s') : null;
-      $DateMaj      = ($entity->getDateMaj() !== null) ? $entity->getDateMaj()->format('Y-m-d H:i:s') : null;
+      $DateMaj = ($entity->getDateMaj() !== null) ? $entity->getDateMaj()->format('Y-m-d H:i:s') : null;
       $tab_toshow[] = array(
-        "id"      => $id,
-        "name"    => 'adn',
-        "code"    => $entity->getCodeAdn(),
+        "id" => $id,
+        "name" => 'adn',
+        "code" => $entity->getCodeAdn(),
         "dateMaj" => $DateMaj,
         "userMaj" => $service->GetUserMajUsername($entity),
       );
@@ -81,11 +79,11 @@ class DashboardController extends AbstractController {
     foreach ($entities_toshow as $entity) {
       $id = $entity->getId();
       //$DateCre = ($entity->getDateCre() !== null) ?  $entity->getDateCre()->format('Y-m-d H:i:s') : null;
-      $DateMaj      = ($entity->getDateMaj() !== null) ? $entity->getDateMaj()->format('Y-m-d H:i:s') : null;
+      $DateMaj = ($entity->getDateMaj() !== null) ? $entity->getDateMaj()->format('Y-m-d H:i:s') : null;
       $tab_toshow[] = array(
-        "id"      => $id,
-        "name"    => 'chromatogramme',
-        "code"    => $entity->getCodeChromato(),
+        "id" => $id,
+        "name" => 'chromatogramme',
+        "code" => $entity->getCodeChromato(),
         "dateMaj" => $DateMaj,
         "userMaj" => $service->GetUserMajUsername($entity),
       );
@@ -99,11 +97,11 @@ class DashboardController extends AbstractController {
     foreach ($entities_toshow as $entity) {
       $id = $entity->getId();
       //$DateCre = ($entity->getDateCre() !== null) ?  $entity->getDateCre()->format('Y-m-d H:i:s') : null;
-      $DateMaj      = ($entity->getDateMaj() !== null) ? $entity->getDateMaj()->format('Y-m-d H:i:s') : null;
+      $DateMaj = ($entity->getDateMaj() !== null) ? $entity->getDateMaj()->format('Y-m-d H:i:s') : null;
       $tab_toshow[] = array(
-        "id"      => $id,
-        "name"    => 'collecte',
-        "code"    => $entity->getCodeCollecte(),
+        "id" => $id,
+        "name" => 'collecte',
+        "code" => $entity->getCodeCollecte(),
         "dateMaj" => $DateMaj,
         "userMaj" => $service->GetUserMajUsername($entity),
       );
@@ -117,11 +115,11 @@ class DashboardController extends AbstractController {
     foreach ($entities_toshow as $entity) {
       $id = $entity->getId();
       //$DateCre = ($entity->getDateCre() !== null) ?  $entity->getDateCre()->format('Y-m-d H:i:s') : null;
-      $DateMaj      = ($entity->getDateMaj() !== null) ? $entity->getDateMaj()->format('Y-m-d H:i:s') : null;
+      $DateMaj = ($entity->getDateMaj() !== null) ? $entity->getDateMaj()->format('Y-m-d H:i:s') : null;
       $tab_toshow[] = array(
-        "id"      => $id,
-        "name"    => 'individu',
-        "code"    => $entity->getCodeIndTriMorpho(),
+        "id" => $id,
+        "name" => 'individu',
+        "code" => $entity->getCodeIndTriMorpho(),
         "dateMaj" => $DateMaj,
         "userMaj" => $service->GetUserMajUsername($entity),
       );
@@ -135,11 +133,11 @@ class DashboardController extends AbstractController {
     foreach ($entities_toshow as $entity) {
       $id = $entity->getId();
       //$DateCre = ($entity->getDateCre() !== null) ?  $entity->getDateCre()->format('Y-m-d H:i:s') : null;
-      $DateMaj      = ($entity->getDateMaj() !== null) ? $entity->getDateMaj()->format('Y-m-d H:i:s') : null;
+      $DateMaj = ($entity->getDateMaj() !== null) ? $entity->getDateMaj()->format('Y-m-d H:i:s') : null;
       $tab_toshow[] = array(
-        "id"      => $id,
-        "name"    => 'individulame',
-        "code"    => $entity->getCodeLameColl(),
+        "id" => $id,
+        "name" => 'individulame',
+        "code" => $entity->getCodeLameColl(),
         "dateMaj" => $DateMaj,
         "userMaj" => $service->GetUserMajUsername($entity),
       );
@@ -153,11 +151,11 @@ class DashboardController extends AbstractController {
     foreach ($entities_toshow as $entity) {
       $id = $entity->getId();
       //$DateCre = ($entity->getDateCre() !== null) ?  $entity->getDateCre()->format('Y-m-d H:i:s') : null;
-      $DateMaj      = ($entity->getDateMaj() !== null) ? $entity->getDateMaj()->format('Y-m-d H:i:s') : null;
+      $DateMaj = ($entity->getDateMaj() !== null) ? $entity->getDateMaj()->format('Y-m-d H:i:s') : null;
       $tab_toshow[] = array(
-        "id"      => $id,
-        "name"    => 'lotmateriel',
-        "code"    => $entity->getCodeLotMateriel(),
+        "id" => $id,
+        "name" => 'lotmateriel',
+        "code" => $entity->getCodeLotMateriel(),
         "dateMaj" => $DateMaj,
         "userMaj" => $service->GetUserMajUsername($entity),
       );
@@ -171,11 +169,11 @@ class DashboardController extends AbstractController {
     foreach ($entities_toshow as $entity) {
       $id = $entity->getId();
       //$DateCre = ($entity->getDateCre() !== null) ?  $entity->getDateCre()->format('Y-m-d H:i:s') : null;
-      $DateMaj      = ($entity->getDateMaj() !== null) ? $entity->getDateMaj()->format('Y-m-d H:i:s') : null;
+      $DateMaj = ($entity->getDateMaj() !== null) ? $entity->getDateMaj()->format('Y-m-d H:i:s') : null;
       $tab_toshow[] = array(
-        "id"      => $id,
-        "name"    => 'lotmaterielext',
-        "code"    => $entity->getCodeLotMaterielExt(),
+        "id" => $id,
+        "name" => 'lotmaterielext',
+        "code" => $entity->getCodeLotMaterielExt(),
         "dateMaj" => $DateMaj,
         "userMaj" => $service->GetUserMajUsername($entity),
       );
@@ -189,11 +187,11 @@ class DashboardController extends AbstractController {
     foreach ($entities_toshow as $entity) {
       $id = $entity->getId();
       //$DateCre = ($entity->getDateCre() !== null) ?  $entity->getDateCre()->format('Y-m-d H:i:s') : null;
-      $DateMaj      = ($entity->getDateMaj() !== null) ? $entity->getDateMaj()->format('Y-m-d H:i:s') : null;
+      $DateMaj = ($entity->getDateMaj() !== null) ? $entity->getDateMaj()->format('Y-m-d H:i:s') : null;
       $tab_toshow[] = array(
-        "id"      => $id,
-        "name"    => 'motu',
-        "code"    => $entity->getLibelleMotu(),
+        "id" => $id,
+        "name" => 'motu',
+        "code" => $entity->getLibelleMotu(),
         "dateMaj" => $DateMaj,
         "userMaj" => $service->GetUserMajUsername($entity),
       );
@@ -207,11 +205,11 @@ class DashboardController extends AbstractController {
     foreach ($entities_toshow as $entity) {
       $id = $entity->getId();
       //$DateCre = ($entity->getDateCre() !== null) ?  $entity->getDateCre()->format('Y-m-d H:i:s') : null;
-      $DateMaj      = ($entity->getDateMaj() !== null) ? $entity->getDateMaj()->format('Y-m-d H:i:s') : null;
+      $DateMaj = ($entity->getDateMaj() !== null) ? $entity->getDateMaj()->format('Y-m-d H:i:s') : null;
       $tab_toshow[] = array(
-        "id"      => $id,
-        "name"    => 'pcr',
-        "code"    => $entity->getCodePcr(),
+        "id" => $id,
+        "name" => 'pcr',
+        "code" => $entity->getCodePcr(),
         "dateMaj" => $DateMaj,
         "userMaj" => $service->GetUserMajUsername($entity),
       );
@@ -225,11 +223,11 @@ class DashboardController extends AbstractController {
     foreach ($entities_toshow as $entity) {
       $id = $entity->getId();
       //$DateCre = ($entity->getDateCre() !== null) ?  $entity->getDateCre()->format('Y-m-d H:i:s') : null;
-      $DateMaj      = ($entity->getDateMaj() !== null) ? $entity->getDateMaj()->format('Y-m-d H:i:s') : null;
+      $DateMaj = ($entity->getDateMaj() !== null) ? $entity->getDateMaj()->format('Y-m-d H:i:s') : null;
       $tab_toshow[] = array(
-        "id"      => $id,
-        "name"    => 'sequenceassemblee',
-        "code"    => $entity->getCodeSqcAss(),
+        "id" => $id,
+        "name" => 'sequenceassemblee',
+        "code" => $entity->getCodeSqcAss(),
         "dateMaj" => $DateMaj,
         "userMaj" => $service->GetUserMajUsername($entity),
       );
@@ -243,11 +241,11 @@ class DashboardController extends AbstractController {
     foreach ($entities_toshow as $entity) {
       $id = $entity->getId();
       //$DateCre = ($entity->getDateCre() !== null) ?  $entity->getDateCre()->format('Y-m-d H:i:s') : null;
-      $DateMaj      = ($entity->getDateMaj() !== null) ? $entity->getDateMaj()->format('Y-m-d H:i:s') : null;
+      $DateMaj = ($entity->getDateMaj() !== null) ? $entity->getDateMaj()->format('Y-m-d H:i:s') : null;
       $tab_toshow[] = array(
-        "id"      => $id,
-        "name"    => 'sequenceassembleeext',
-        "code"    => $entity->getCodeSqcAssExt(),
+        "id" => $id,
+        "name" => 'sequenceassembleeext',
+        "code" => $entity->getCodeSqcAssExt(),
         "dateMaj" => $DateMaj,
         "userMaj" => $service->GetUserMajUsername($entity),
       );
@@ -261,73 +259,43 @@ class DashboardController extends AbstractController {
     foreach ($entities_toshow as $entity) {
       $id = $entity->getId();
       //$DateCre = ($entity->getDateCre() !== null) ?  $entity->getDateCre()->format('Y-m-d H:i:s') : null;
-      $DateMaj      = ($entity->getDateMaj() !== null) ? $entity->getDateMaj()->format('Y-m-d H:i:s') : null;
+      $DateMaj = ($entity->getDateMaj() !== null) ? $entity->getDateMaj()->format('Y-m-d H:i:s') : null;
       $tab_toshow[] = array(
-        "id"      => $id,
-        "name"    => 'station',
-        "code"    => $entity->getCodeStation(),
+        "id" => $id,
+        "name" => 'station',
+        "code" => $entity->getCodeStation(),
         "dateMaj" => $DateMaj,
         "userMaj" => $service->GetUserMajUsername($entity),
       );
     }
 
     return $this->render('Core/dashboard/index.html.twig', array(
-      'nbCollecte'             => $nbcollectes,
-      'nbStation'              => $nbstations,
-      'nbLotMateriel'          => $nbLotMateriel,
-      'nbLotMaterielExt'       => $nbLotMaterielExt,
-      'nbIndividu'             => $nbIndividu,
-      'nbIndividuLame'         => $nbIndividuLame,
-      'nbAdn'                  => $nbAdn,
-      'nbPcr'                  => $nbPcr,
-      'nbChromatogramme'       => $nbChromatogramme,
-      'nbSequenceAssemblee'    => $nbSequenceAssemblee,
+      'nbCollecte' => $nbcollectes,
+      'nbStation' => $nbstations,
+      'nbLotMateriel' => $nbLotMateriel,
+      'nbLotMaterielExt' => $nbLotMaterielExt,
+      'nbIndividu' => $nbIndividu,
+      'nbIndividuLame' => $nbIndividuLame,
+      'nbAdn' => $nbAdn,
+      'nbPcr' => $nbPcr,
+      'nbChromatogramme' => $nbChromatogramme,
+      'nbSequenceAssemblee' => $nbSequenceAssemblee,
       'nbSequenceAssembleeExt' => $nbSequenceAssembleeExt,
-      'nbMotu'                 => $nbMotu,
-      'nbMotuSqcAss'           => $nbMotuSqcAss,
-      'nbMotuSqcAssExt'        => $nbMotuSqcAssExt,
-      'nbBoite'                => $nbBoite,
-      'nbSource'               => $nbSource,
-      'nbTaxon'                => $nbTaxon,
-      'entities'               => $tab_toshow,
+      'nbMotu' => $nbMotu,
+      'nbMotuSqcAss' => $nbMotuSqcAss,
+      'nbMotuSqcAssExt' => $nbMotuSqcAssExt,
+      'nbBoite' => $nbBoite,
+      'nbSource' => $nbSource,
+      'nbTaxon' => $nbTaxon,
+      'entities' => $tab_toshow,
     ));
   }
 
   /**
-   * @Route("/mapstations/", name="mapstations", methods={"POST"})
+   * @Route("/legal/", name="legal_notices", methods={"GET"})
    */
-  public function geoCoords(Request $request) {
-    $data                  = $request->request;
-    $latitude              = $data->get('latitude');
-    $longitude             = $data->get('longitude');
-    $diffLatitudeLongitude = 1;
-    //
-    $em = $this->getDoctrine()->getManager();
-
-    $tab_toshow      = [];
-    $entities_toshow = $em->getRepository("App:Station")->createQueryBuilder('station')
-      ->getQuery()
-      ->getResult();
-    $nb_entities = count($entities_toshow);
-    foreach ($entities_toshow as $entity) {
-      $id = $entity->getId();
-      //$DateCre = ($entity->getDateCre() !== null) ?  $entity->getDateCre()->format('Y-m-d H:i:s') : null;
-      //$DateMaj = ($entity->getDateMaj() !== null) ?  $entity->getDateMaj()->format('Y-m-d H:i:s') : null;
-      $tab_toshow[] = array(
-        "id"                  => $id, "station.id" => $id,
-        "station.codeStation" => $entity->getCodeStation(),
-        "station.nomStation"  => $entity->getNomStation(),
-        "commune.codeCommune" => $entity->getCommuneFk()->getCodeCommune(),
-        "pays.codePays"       => $entity->getPaysFk()->getCodePays(),
-        "station.latDegDec"   => $entity->getLatDegDec(),
-        "station.longDegDec"  => $entity->getLongDegDec(),
-      );
-    }
-
-    return new JsonResponse(array(
-      'stations'  => $tab_toshow,
-      'latitude'  => $latitude,
-      'longitude' => $longitude,
-    ));
+  public function legalNotices(Request $request) {
+    $locale = $request->getLocale();
+    return $this->render('misc/legal-notices.html.twig');
   }
 }
