9c59242c39099ca0b72e02795fcb9daa2df2e24a
lsdch <louis.duchemin@univ-lyon1.fr>
2021-02-18 18:28:46 +0100

    [Forms] Fix AddUserDateFields event listener to preserve empty user values

--- src/Form/EventListener/AddUserDateFields.php
+++ src/Form/EventListener/AddUserDateFields.php
@@ -2,57 +2,59 @@
 
 namespace App\Form\EventListener;
 
-use App\Form\Enums\Action;
-use DateTime;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use DateTime;
+use App\Form\Enums\Action;
 
-class AddUserDateFields implements EventSubscriberInterface
-{
+class AddUserDateFields implements EventSubscriberInterface {
   protected $security;
 
-  public function __construct(Security $security)
-  {
+  public function __construct(Security $security) {
     $this->security = $security;
   }
 
-  public static function getSubscribedEvents()
-  {
+  public static function getSubscribedEvents() {
     return [
-      FormEvents::PRE_SET_DATA  => 'onPreSetData',
-      FormEvents::SUBMIT  => 'onSubmit',
+      FormEvents::PRE_SET_DATA => 'onPreSetData',
+      FormEvents::SUBMIT => 'onSubmit',
     ];
   }
 
-  public function onSubmit(FormEvent $event)
-  {
+  public function onSubmit(FormEvent $event) {
     $data = $event->getData();
 
+    $user = $this->security->getUser();
+
+    /* Do not replace creator user if creation date is defined :
+    it means that the record was already existing,
+    but creator user was not known*/
+    $data->setUserCre($data->getUserCre() || $data->getDateCre()
+      ? $data->getUserCre()
+      : $user->getId());
+    $data->setUserMaj($user->getId());
+
+    // This is why we are setting the date *after* setting the user
     $now = new DateTime();
     $data->setDateCre($data->getDateCre() ?: $now);
     $data->setDateMaj($now);
 
-    $user = $this->security->getUser();
-    $data->setUserCre($data->getUserCre() ?: $user->getId());
-    $data->setUserMaj($user->getId());
-
     $event->setData($data);
   }
 
-  public function onPreSetData(FormEvent $event)
-  {
+  public function onPreSetData(FormEvent $event) {
     $form = $event->getForm();
     $form_type = $form->getConfig()->getOption("action_type");
 
     if ($form_type == Action::show()) {
       $form->add('dateCre', DateTimeType::class, [
-        'widget' => 'single_text'
+        'widget' => 'single_text',
       ])
         ->add('dateMaj', DateTimeType::class, [
-          'widget' => 'single_text'
+          'widget' => 'single_text',
         ]);
     }
   }
