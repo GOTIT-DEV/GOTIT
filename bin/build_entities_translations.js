29dfe3a811596ca06852c2793009d46b052b777b
lsdch <louis.duchemin@univ-lyon1.fr>
2021-03-10 15:37:14 +0100

    Update tables/fields translations

--- bin/build_entities_translations.js
+++ bin/build_entities_translations.js
@@ -3,12 +3,15 @@ const Parser = require('papaparse')
 const yaml = require('js-yaml')
 fs = require('fs')
 
-const argv = yargs
-  .usage('Usage : $0 <CSV translations>')
-  .demandCommand(1)
-  .argv
 
-const csv_path = argv._[0]
+if (require.main === module) {
+  const argv = yargs
+    .usage('Usage : $0 <CSV translations>')
+    .demandCommand(1)
+    .argv
+  const csv_path = argv._[0]
+  csv_file_to_yaml_translations(csv_path)
+}
 
 function output_translations(translations, locale, key) {
   const output_path = `translations/${key}.${locale}.yml`
@@ -17,35 +20,41 @@ function output_translations(translations, locale, key) {
   fs.writeFileSync(output_path, yamlStr, 'utf8')
 }
 
-console.log(`Parsing ${csv_path}`)
+function csv_file_to_yaml_translations(csv_path) {
+  console.log(`Parsing ${csv_path}`)
 
-fs.readFile(csv_path, 'utf8', function (err, data) {
-  items = Parser.parse(data, { header: true, skipEmptyLines: true })
-  translations = items.data.reduce((acc, item) => {
-    acc.en.tables[item.table] = item.table_en
-    acc.fr.tables[item.table] = item.table_fr
+  fs.readFile(csv_path, 'utf8', function (err, data) {
+    items = Parser.parse(data, { header: true, skipEmptyLines: true })
+    translations = items.data.reduce((acc, item) => {
+      acc.en.tables[item.table] = item.table_en
+      acc.fr.tables[item.table] = item.table_fr
 
-    if (!(item.table in acc.en.fields)) {
-      acc.en.fields[item.table] = {}
-      acc.fr.fields[item.table] = {}
-    }
-    acc.en.fields[item.table][item.field] = item.field_en
-    acc.fr.fields[item.table][item.field] = item.field_fr
+      if (!(item.table in acc.en.fields)) {
+        acc.en.fields[item.table] = {}
+        acc.fr.fields[item.table] = {}
+      }
+      acc.en.fields[item.table][item.field] = item.field_en
+      acc.fr.fields[item.table][item.field] = item.field_fr
 
-    return acc
-  }, {
-    en: {
-      tables: {}, fields: {}
-    },
-    fr: {
-      tables: {}, fields: {}
-    },
-  })
-
-  const locales = ["en", "fr"]
-  locales.forEach(locale => {
-    ['tables', 'fields'].forEach(key => {
-      output_translations(translations, locale, key)
+      return acc
+    }, {
+      en: {
+        tables: {}, fields: {}
+      },
+      fr: {
+        tables: {}, fields: {}
+      },
     })
-  });
-})
+
+    const locales = ["en", "fr"]
+    locales.forEach(locale => {
+      ['tables', 'fields'].forEach(key => {
+        output_translations(translations, locale, key)
+      })
+    });
+  })
+}
+
+module.exports = csv_file_to_yaml_translations
+
+
