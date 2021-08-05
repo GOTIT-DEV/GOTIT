56f247d504a2cb60d106c30fddcdae367aad52f6
Louis Duchemin <ls.duchemin@gmail.com>
2021-08-05 11:42:54 +0200

    Setup REST API dependencies

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,7 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
 ];
---
+++ src/Controller/API/DNAController.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Controller\API;
+
+class DNAController {
+
+}
