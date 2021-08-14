0c8392396141acf369ad91ff7539073b9aeef59d
Louis Duchemin <ls.duchemin@gmail.com>
2021-08-14 08:28:41 +0200

    Setup Vue JS initialization for index pages

--- assets/Core/js/index.js
+++ assets/Core/js/index.js
@@ -1,45 +1,29 @@
-// SOURCE : http://www.jqueryscript.net/table/jQuery-Plugin-To-Convert-HTML-Table-To-CSV-tabletoCSV.html
-jQuery.fn.tableToCSV = function () {
-  var clean_text = function (text) {
-    text = text.replace(/"/g, '""');
-    return '"' + text + '"';
-  };
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "bootstrap-select/dist/css/bootstrap-select.css";
+import "~SpeciesSearch/css/multiselect.less";
 
-  $(this).each(function () {
-    var table = $(this);
-    var caption = $(this).find('caption').text();
-    var title = [];
-    var rows = [];
+import Vue from "vue";
+import { BootstrapVue } from "bootstrap-vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-    $(this).find('tr').each(function () {
-      var data = [];
-      $(this).find('th').each(function () {
-        var text = clean_text($(this).text());
-        title.push(text);
-      });
-      $(this).find('td').each(function () {
-        var text = clean_text($(this).text());
-        data.push(text);
-      });
-      data = data.join(";");
-      rows.push(data);
-    });
-    title = title.join(";");
-    rows = rows.join("\n");
+import filters from "~SpeciesSearch/js/vue-filters";
+import i18n from "~Core/js/i18n";
 
-    var csv = title + rows;
-    var uri = 'data:text/csv;charset=utf-8,' + encodeURIComponent(csv);
-    var download_link = document.createElement('a');
-    download_link.href = uri;
-    var ts = new Date().getTime();
-    if (caption == "") {
-      download_link.download = ts + ".csv";
-    } else {
-      download_link.download = caption + "-" + ts + ".csv";
-    }
-    document.body.appendChild(download_link);
-    download_link.click();
-    document.body.removeChild(download_link);
-  });
+import moment from "moment";
+moment.locale(Translator.locale);
 
-};
+Vue.component("FontAwesomeIcon", FontAwesomeIcon);
+
+Vue.use(BootstrapVue);
+Vue.mixin({
+  i18n,
+  methods: {
+    generateRoute(route, args) {
+      return Routing.generate(route, args);
+    },
+  },
+});
+
+Object.entries(filters).forEach(([key, filter]) => {
+  Vue.filter(key, filter);
+});
