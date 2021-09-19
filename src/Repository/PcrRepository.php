8c85cf94f49b6be3a588f569fc36ada2bc42e34b
Louis Duchemin <louis.duchemin@etu.univ-lyon1.fr>
2021-09-19 22:10:20 +0200

    PcrRepository implementation

---
+++ src/Repository/PcrRepository.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Pcr;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+final class PcrRepository extends AbstractRepository {
+
+  public function __construct(ManagerRegistry $managerRegistry, ValidatorInterface $validator) {
+    parent::__construct($managerRegistry, Pcr::class, $validator);
+    $this->fetchJoinQueryBuilder = $this->createQueryBuilder('pcr')
+      ->select('pcr, dna, spec, prod, person, inst, pcr')
+      ->join('pcr.dnaFk', 'dna')
+      ->leftJoin('pcr.pcrProducers', 'prod')
+      ->leftJoin('prod.personFk', 'person')
+      ->leftJoin('person.institutionFk', 'inst');
+  }
+
+  public function getColumnKey(string $column): string {
+    $column = $this->toCamelCase($column);
+    switch ($column) {
+    case "dnaFk":
+      $column = "dna.code";
+      break;
+    default:
+      $column = 'pcr.' . $column;
+    }
+    return $column;
+  }
+}
