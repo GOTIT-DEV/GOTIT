58ba18f72693a62b0bad1219efe34034a5519c46
Louis Duchemin <ls.duchemin@gmail.com>
2021-09-28 08:43:56 +0200

    [Form] Fixes DNA embed type

--- src/Form/EmbedTypes/DnaEmbedType.php
+++ src/Form/EmbedTypes/DnaEmbedType.php
@@ -2,32 +2,29 @@
 
 namespace App\Form\EmbedTypes;
 
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\FormBuilderInterface;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class DnaEmbedType extends AbstractType {
-  /**
-   * {@inheritdoc}
-   */
-  public function buildForm(FormBuilderInterface $builder, array $options) {
-    $builder->add('code');
-  }
+	/**
+	 * {@inheritdoc}
+	 */
+	public function configureOptions(OptionsResolver $resolver) {
+		$resolver->setDefaults([
+			'class' => 'App:Dna',
+			'multiple' => false,
+			'expanded' => false,
+			'label' => false,
+			'choice_label' => 'code',
+			'query_builder' => function (EntityRepository $er) {
+				return $er->createQueryBuilder('dna')->orderBy('dna.code', 'ASC');
+			},
+		]);
+	}
 
-  /**
-   * {@inheritdoc}
-   */
-  public function configureOptions(OptionsResolver $resolver) {
-    $resolver->setDefaults(array(
-      'data_class' => 'App\Entity\Dna',
-    ));
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getBlockPrefix() {
-    return 'dna';
-  }
-
+	public function getParent() {
+		return EntityType::class;
+	}
 }
