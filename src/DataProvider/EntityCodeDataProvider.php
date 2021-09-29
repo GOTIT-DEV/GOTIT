321836a84e4a99eb10f80c0c88941cde5e3d43ae
Louis Duchemin <ls.duchemin@gmail.com>
2021-09-29 17:27:26 +0200

    [API] Setup DataProvider to get entities by code

---
+++ src/DataProvider/EntityCodeDataProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DataProvider;
+
+use ApiPlatform\Core\DataProvider\ItemDataProviderInterface;
+use ApiPlatform\Core\DataProvider\RestrictedDataProviderInterface;
+use Doctrine\Persistence\ManagerRegistry;
+
+class EntityCodeDataProvider implements ItemDataProviderInterface, RestrictedDataProviderInterface {
+  public function __construct(private ManagerRegistry $managerRegistry) {
+  }
+
+  public function supports(string $resourceClass, ?string $operationName = null, array $context = []): bool {
+    return isset(class_uses($resourceClass)['CompositeCodeEntityTrait'])
+            || property_exists($resourceClass, 'code');
+  }
+
+  public function getItem(string $resourceClass, $id, ?string $operationName = null, array $context = []) {
+    $manager = $this->managerRegistry->getManagerForClass($resourceClass);
+    $repository = $manager->getRepository($resourceClass);
+    $useIntegerId = is_numeric($id);
+
+    return $useIntegerId ? $repository->find($id) : $repository->findOneBy(['code' => $id]);
+  }
+}
