eb4d117d47d2712c5ae96774694e52538bc5f1d4
GRISON Philippe <philippe.grison@mnhn.fr>
2023-01-20 14:10:34 +0100

    add message if $ caracter is used

--- assets/Core/forms/js/voc.js
+++ assets/Core/forms/js/voc.js
@@ -4,6 +4,9 @@ $(() => {
   const $code = $form.find('#bbees_e3sbundle_voc_code')
   if ($form.data('action') == "new") {
     $code.keyup(function (e) {
+      if ($code.val().includes('$')) {
+          alert(' ! the $ character is not allowed. Please change for another')
+      }
       const codeVoc = $code.val().replace(/\$/g, '');
       $code.val(codeVoc);
     })
