8cb0d3699d2902ddc9c8ea671c561120a0ff559b
GRISON Philippe <philippe.grison@mnhn.fr>
2023-01-24 15:39:03 +0100

    interdiction du caractère  $  (en mode création et edit) dans le code Boite - 2

--- assets/Core/forms/js/box.js
+++ assets/Core/forms/js/box.js
@@ -4,11 +4,19 @@ $(() => {
   const $code = $form.find('#bbees_e3sbundle_boite_codeBoite')
   if ($form.data('action') == "new") {
     $code.keyup(function (e) {
-      const codeBoite = $code.val().replace(/ /g, '_');
       if ($code.val().includes('$')) {
           alert(' ! the $ character is not allowed. Please change for another')
-          const codeBoite = $code.val().replace(/\$/g, '');
       }
+      const codeBoite = $code.val().replace(/\$/g, '').replace(/ /g, '_');
+      $code.val(codeBoite);
+    })
+  }
+  if ($form.data('action') == "edit") {
+    $code.keyup(function (e) {
+      if ($code.val().includes('$')) {
+          alert(' ! the $ character is not allowed. Please change for another')
+      }
+      const codeBoite = $code.val().replace(/\$/g, '');
       $code.val(codeBoite);
     })
   }
